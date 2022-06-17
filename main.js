afb68101fc94bd63dda841b59df8cdff9a6326fd
nataly <nataly.angulo530@gmail.com>
2022-06-17 06:38:30 +0100

    changes to accessibility nav menu

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 $(document).ready(function(){
-    // Alter Font-Size - NEED TO SET LIMITS ON FONT-SIZE
-    $(".increaseFont,.decreaseFont").click(function(){
-    var type=  $(this).val();
-    var curFontSize = $('.data').css('font-size');
-     if(type=='increase'){
-      $('.data').css('font-size', parseInt(curFontSize)+1);
-      }
-     else{
-      $('.data').css('font-size', parseInt(curFontSize)-1);
-     }  
-       });
+    // dropdown font
+    $(".increaseFont").click(function(){
+    var type= $(this).val();
+    console.log(type)
+    $('.data').css('font-size', type);
+    });
     // Background-color and font-color change
     $(function() {  
       $('#highRatio').click(function(){
@@ -29,11 +24,12 @@ $(document).ready(function(){
           $('.data').css('color', '#E5C54A');
         });
       });
-    // FingerSpell Language
+    // FingerSpell
     $(function() {  
       $('#fingerspell').click(function(){
           $('.data').css('font-family', 'Gallaudet');
-          $('.data').css('font-size', '40px');
+          $('.data').css('font-size', '50px');
+          $('.data').css('font-weight', 'normal');
         });
       });
       
@@ -42,23 +38,45 @@ $(document).ready(function(){
   //DATA
   $.getJSON('../backend/data.json', function(data){
     let TopSellers = data.TopSellers;
+    // console.log(TopSellers)
     $.each(TopSellers, function(index, value){
       // console.log(index, value)
-      console.log(value.name)
-      $('#topSellers').append(`<figure><a href="" alt="contains Image of product"><img src=${value.img} alt="image of products available"></a></img><figcaption><ul><li><h2><a href="" alt="contains Name of product"></h2>${value.name}</a></li><li>£ ${value.price}</li></ul></figcaption></figure>`)
+      // console.log(value.name)
+      $('#topSellers').append(`
+      <figure class="data">
+        <a href="" alt="contains Image of product"><img src=${value.img} alt="image of products available"></a>
+        <figcaption>
+        <ul>
+        <li><h2><a href="" alt="contains Name of product"></h2>${value.name}</a></li>
+        <li>£ ${value.price}</li></ul>
+        </figcaption>
+      </figure>`)
+    })
+
+
+    // SEARCHBAR
+    $('#searchBar').keyup(function(e){
+      let input = $('#searchBar').val()
+      input = input.toLowerCase();
+      $.each(data, function(index, value){
+        for(var key in value) {
+          let dataValue = value[key]
+            if (dataValue.name.toLowerCase().indexOf(input)!= -1) {
+              $('.searchBarResults').html(`
+              <figure class="data">
+                  <img src="${dataValue.img} alt="image of products available>
+              </figure>
+              <figcaption>${dataValue.name}</figcaption>
+              <figcaption>£${dataValue.price}</figcaption>
+              <figcaption>${dataValue.category}</figcaption>`)
+              console.log(dataValue.category)
+            }else(console.log("no work"))
+        }
+      });
+      console.log(input)
+      e.preventDefault();
     })
   })
-
-
-// MODALS
-
-
-
-
-
-
-
-
-
+  
 
 });
