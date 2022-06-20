87789dbde165f4909b13ee8cdaa8a2749a539751
nataly <nataly.angulo530@gmail.com>
2022-06-20 14:06:48 +0100

    new changes

--- links.md
+++ links.md
@@ -6,3 +6,16 @@ https://www.achrafothman.net/aslsmt/corpus/sample-corpus-asl-en.en
 
 
 TO-DO: 
+- [ ] mobile view
+- [ ] Carousel functionality
+- [ ] cart functionality
+- [ ] Design
+- [ ] Accessibility checks
+- [ ] Modals
+- [ ] Debugging
+- [ ] Write up - 3 weeks needed
+- [ ] font-size limit set
+
+Maybe:
+- [ ] Filters????
+- [ ] Animations?????
--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
     // Alter Font-Size - NEED TO SET LIMITS ON FONT-SIZE
-    $(".increaseFont,.decreaseFont").click(function(){
-    var type=  $(this).val();
-    var curFontSize = $('.data').css('font-size');
-     if(type=='increase'){
-      $('.data').css('font-size', parseInt(curFontSize)+7);
-      }else{
-      $('.data').css('font-size', parseInt(curFontSize)-7);
-     }  
-       });
+    // $(".increaseFont,.decreaseFont").click(function(){
+    // var type=  $(this).val();
+    // var curFontSize = $('.data').css('font-size');
+    //  if(type=='increase'){
+    //   $('.data').css('font-size', parseInt(curFontSize)+7);
+    //   }else{
+    //   $('.data').css('font-size', parseInt(curFontSize)-7);
+    //  }  
+    //    });
 
-    // // dropdown font
-    // $(".increaseFont").click(function(){
-    // var type= $(this).val();
-    // console.log(type)
-    // $('.data').css('font-size', type);
-    // });
+    // dropdown font
+    $(".increaseFont").click(function(){
+    var type= $(this).val();
+    console.log(type)
+    $('.data').css('font-size', type);
+    });
     // Background-color and font-color change
     $(function() {  
       $('#highRatio').click(function(){
@@ -47,45 +47,54 @@ $(document).ready(function(){
       
 
 // Select sections to increase size and decrease
-  //DATA
-  $.getJSON('../backend/data.json', function(data){
-    let TopSellers = data.TopSellers;
-    $.each(TopSellers, function(index, value){
-      $('#topSellers').append(`
+$.getJSON('../backend/data.json', function(data){
+  //TOP SELLERS Home page
+  let TopSellers = data.TopSellers;
+  $.each(TopSellers, function(index, value){
+    $('#topSellers').append(`
+    <figure class="data">
+      <a href="" alt="contains Image of product"><img src=${value.img} alt="image of products available"></a>
+      <figcaption>
+      <ul>
+      <li><h2><a href="" alt="contains Name of product"></h2>${value.name}</a></li>
+      <li>£ ${value.price}</li></ul>
+      </figcaption>
+    </figure>`)
+  })
+
+  $.each(data, function(index, value){
+    let dataValue = value[0]
+    console.log(dataValue)
+      $('#catalogLayout').append(`
       <figure class="data">
-        <a href="" alt="contains Image of product"><img src=${value.img} alt="image of products available"></a>
+        <a href="" alt="contains Image of product"><img src=${dataValue.img} alt="image of products available"></a>
         <figcaption>
-        <ul>
-        <li><h2><a href="" alt="contains Name of product"></h2>${value.name}</a></li>
-        <li>£ ${value.price}</li></ul>
+          <h2><a href="" alt="contains Name of product"></h2>${dataValue.category}</a>
         </figcaption>
       </figure>`)
-    })
-
-
-    // SEARCHBAR
-    $('#searchBar').keyup(function(e){
-      let input = $('#searchBar').val()
-      input = input.toLowerCase();
-      $.each(data, function(index, value){
-        for(var key in value) {
-          let dataValue = value[key]
-            if (dataValue.name.toLowerCase().indexOf(input)!= -1) {
-              $('.searchBarResults').html(`
-              <figure class="data">
-                  <img src="${dataValue.img} alt="image of products available>
-              </figure>
-              <figcaption>${dataValue.name}</figcaption>
-              <figcaption>£${dataValue.price}</figcaption>
-              <figcaption>${dataValue.category}</figcaption>`)
-              console.log(dataValue.category)
-            }else(console.log("no work"))
-        }
-      });
-      console.log(input)
-      e.preventDefault();
-    })
   })
-  
 
+  // SEARCHBAR
+  $('#searchBar').keyup(function(e){
+    let input = $('#searchBar').val()
+    input = input.toLowerCase();
+    $.each(data, function(index, value){
+      for(var key in value) {
+        let dataValue = value[key]
+          if (dataValue.name.toLowerCase().indexOf(input)!= -1) {
+            $('.searchBarResults').html(`
+            <figure class="data">
+                <img src="${dataValue.img} alt="image of products available>
+            </figure>
+            <figcaption>${dataValue.name}</figcaption>
+            <figcaption>£${dataValue.price}</figcaption>
+            <figcaption>${dataValue.category}</figcaption>`)
+            console.log(dataValue.category)
+          }else(console.log("no work"))
+      }
+    });
+    console.log(input)
+    e.preventDefault();
+  })
+})
 });
