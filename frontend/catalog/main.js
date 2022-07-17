375658b1812715ec538d3e8ebce144cb0c88902b
nataly <nataly.angulo530@gmail.com>
2022-07-17 11:52:58 +0100

    making changes to catalog page

--- frontend/catalog/main.js
+++ frontend/catalog/main.js
@@ -2,17 +2,42 @@ $(document).ready(function(){
     const db_url = 'http://localhost:3000'
     $.getJSON(`${db_url}/getData`, function(data){
         // console.log(data)
+        console.log(data.category)
         $.each(data, function(index, value){
-            let category =  value.category
-            let productImg = value.product[0].img
+          let id = value._id
+          let category =  value.category
+          let productImg = value.product[0].img
             $('#catalog').append(`
             <figure class="data">
-                <a href="" alt="contains Image of product"><img src=${productImg} alt="image of products available"></a>
+                <a id=${category} href="" alt="contains Image of product"><img src=${productImg} alt="image of products available"></a>
                 <figcaption>
                     <h2>
                     <a href="" alt="contains Name of product">${category}</a></h2>
                 </figcaption>
             </figure>`)
+            // ALL CONTENT
+          })
         })
+    $("#Fig Yarrow").click(function(e){
+      e.preventDefault()
+      alert("I am active")
+      console,log("I am active")
     })
+    
+    
+
+
+
+  //MODALS
+  $(".open").click(function(){
+    $(".popup, .popup-content").addClass("active");
+    let dataSelected = $("#topData").val()
+    console.log(dataSelected)
+    $(".popup-content").append(dataSelected)
+    console.log("hello?")
+  });
+  $(".close, .popup").click(function(){
+    $(".popup, .popup-content").removeClass("active");
+  });
+
 })
--- frontend/main.js
+++ frontend/main.js
@@ -1,108 +1,106 @@
 $(document).ready(function(){
   const db_url = 'http://localhost:3000'
-    // Alter Font-Size - NEED TO SET LIMITS ON FONT-SIZE
-    $(".increaseFont,.decreaseFont").click(function(){
-    var type=  $(this).val();
-    var curFontSize = $('.data').css('font-size');
-     if(type=='increase'){
-      $('.data').css('font-size', parseInt(curFontSize)+7);
-      }else{
-      $('.data').css('font-size', parseInt(curFontSize)-7);
-     }  
-       });
-
-    $(function() {  
-      $('#highRatio').click(function(){
-          $('body').css('background-color', 'white');
-          $('.data').css('color', 'black');
-        });
+  // Alter Font-Size - NEED TO SET LIMITS ON FONT-SIZE
+  $(".increaseFont,.decreaseFont").click(function(){
+  var type=  $(this).val();
+  var curFontSize = $('.data').css('font-size');
+    if(type=='increase'){
+    $('.data').css('font-size', parseInt(curFontSize)+7);
+    }else{
+    $('.data').css('font-size', parseInt(curFontSize)-7);
+    }  
       });
-    $(function() {  
-      $('#mediumRatio').click(function(){
-          $('body').css('background-color', '#73A1B9');
-          $('.data').css('color', '#222A2E');
-        });
+  $(function() {  
+    $('#highRatio').click(function(){
+        $('body').css('background-color', 'white');
+        $('.data').css('color', 'black');
       });
-    $(function() {  
-      $('#lowRatio').click(function(){
-          $('body').css('background-color', '#045884');
-          $('.data').css('color', '#E5C54A');
-        });
+    });
+  $(function() {  
+    $('#mediumRatio').click(function(){
+        $('body').css('background-color', '#73A1B9');
+        $('.data').css('color', '#222A2E');
       });
-    // FingerSpell
-    $(function() {  
-      $('#fingerspell').click(function(){
-          $('.data').css('font-family', 'Gallaudet');
-          $('.data').css('font-size', '50px');
-          $('.data').css('font-weight', 'normal');
-          $('.data').css('letter-spacing', '10px');
-        });
+    });
+  $(function() {  
+    $('#lowRatio').click(function(){
+        $('body').css('background-color', '#045884');
+        $('.data').css('color', '#E5C54A');
       });
+    });
+  // FingerSpell
+  $(function() {  
+    $('#fingerspell').click(function(){
+        $('.data').css('font-family', 'Gallaudet');
+        $('.data').css('font-size', '50px');
+        $('.data').css('font-weight', 'normal');
+        $('.data').css('letter-spacing', '10px');
+      });
+    });
       
 
-// Select sections to increase size and decrease
-$.getJSON(`${db_url}/getData`, function(data){
-  console.log([data[0].category])
-  //TOP SELLERS Home page
-  let TopSellers = data[0].product;
-  $.each(TopSellers, function(index, value){
-    console.log(value)
-    $('#topSellers').append(`
-    <figure class="data">
-      <a href="" alt="contains Image of product"><img src=${value.img} alt="image of products available"></a>
-      <figcaption>
-      <ul>
-      <li><h2><a href="" alt="contains Name of product"></h2>${value.name}</a></li>
-      <li>£ ${value.price}</li></ul>
-      </figcaption>
-    </figure>`)
-  })
-
-  $.each(data, function(index, value){
-    let dataValue = value[0]
-    // console.log(dataValue)
-      $('#catalogLayout').append(`
+  // Select sections to increase size and decrease
+  $.getJSON(`${db_url}/getData`, function(data){
+    // console.log([data[0].category])
+    //TOP SELLERS Home page
+    let TopSellers = data[0].product;
+    $.each(TopSellers, function(index, value){
+      // console.log(value)
+      $('#topSellers').append(`
       <figure class="data">
-        <a href="" alt="contains Image of product"><img src=${dataValue.img} alt="image of products available"></a>
+        <a href="" alt="contains Image of product"><img src=${value.img} alt="image of products available"></a>
         <figcaption>
         <ul>
         <li><h2><a href="" alt="contains Name of product"></h2>${value.name}</a></li>
         <li>£ ${value.price}</li></ul>
-        <button>Add to Cart</button>
         </figcaption>
       </figure>`)
     })
 
+    $.each(data, function(index, value){
+      // let dataValue = value
+      // console.log(value)
+        $('#catalogLayout').append(`
+        <figure id="topData" class="data">
+          <a href="" alt="contains Image of product"><img src=${value.img} alt="image of products available"></a>
+          <figcaption>
+          <ul>
+          <li><h2><a href="" alt="contains Name of product"></h2>${value.name}</a></li>
+          <li>£ ${value.price}</li></ul>
+          <button>Add to Cart</button>
+          </figcaption>
+        </figure>`)
+      })
 
-    // SEARCHBAR
-    $('#searchBar').keyup(function(e){
-      let input = $('#searchBar').val()
-      input = input.toLowerCase();
-      $.each(data, function(index, value){
-        for(var key in value) {
-          let dataValue = value[key]
-            if (dataValue.name.toLowerCase().indexOf(input)!= -1) {
-              $('.searchBarResults').html(`
-              <figure class="data">
-                  <img src="${dataValue.img} alt="image of products available>
-              </figure>
-              <figcaption>${dataValue.name}</figcaption>
-              <figcaption>£${dataValue.price}</figcaption>
-              <figcaption>${dataValue.category}</figcaption>`)
-              console.log(dataValue.category)
-            }
-        }
-      });
-      console.log(input)
-      e.preventDefault();
+
+      // SEARCHBAR
+      $('#searchBar').keyup(function(e){
+        let input = $('#searchBar').val()
+        input = input.toLowerCase();
+        $.each(data, function(index, value){
+          for(var key in value) {
+            let dataValue = value[key]
+              if (dataValue.name.toLowerCase().indexOf(input)!= -1) {
+                $('.searchBarResults').html(`
+                <figure class="data">
+                    <img src="${dataValue.img} alt="image of products available>
+                </figure>
+                <figcaption>${dataValue.name}</figcaption>
+                <figcaption>£${dataValue.price}</figcaption>
+                <figcaption>${dataValue.category}</figcaption>`)
+                console.log(dataValue.category)
+              }
+          }
+        });
+        console.log(input)
+        e.preventDefault();
+      })
     })
-  })
 
   // CART FUNCTION
-    $('#cartBtn').click(function(){
-      window.open('/cart/cart.html')
-      console.log("I AM THE CART FUNCTION")
-    })
-    
+  $('#cartBtn').click(function(){
+    window.open('/cart/cart.html')
+    console.log("I AM THE CART FUNCTION")
+  })
+
 })
-// });
