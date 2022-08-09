c58d316be43a280947b41dac99fccb73d5c5af83
nataly <nataly.angulo530@gmail.com>
2022-08-09 18:03:14 +0100

    heroku config port

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const http = require('http');
+// const http = require('http');
 const mongoose = require('mongoose');
 // const Products = require('./models/products')
 // Server set-up
 // const port = 3000;
-const port = http.createServer(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
