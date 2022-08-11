754cefee2bcf5e06ee2fd8e98f067e46927e7a88
nataly <nataly.angulo530@gmail.com>
2022-08-12 00:10:39 +0100

    adding cart modal

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -32,7 +32,18 @@ $.getJSON(`${db_url}`, function(data){
 })
 
 
-
+$("#cart").css("display", "none");
+$('#cartModal').click(function(event){
+  event.preventDefault();
+  console.log("im on catalog?")
+  $('#cart').toggle()
+  
+})
+$('#closeModal').click(function(event){
+  event.preventDefault();
+  $('#cart').css("display: none")
+  console.log("close it damnnit")
+})
 
 
     
