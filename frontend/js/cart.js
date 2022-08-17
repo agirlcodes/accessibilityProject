a102f2e26eef69bdf42c7c77fdc1a082a4b0ac36
nataly <nataly.angulo530@gmail.com>
2022-08-17 22:04:20 +0100

    clearing cart after transaction

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -113,11 +113,14 @@ paypal.Buttons({
 },
 // Finalize the transaction after payer approval
 onApprove: (data, actions) => {
+    // alert("Your payment has gone through")
     return actions.order.capture().then(function(orderData) {
         // Successful capture! For dev/demo purposes:
         console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
         const transaction = orderData.purchase_units[0].payments.captures[0];
         alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
+        localStorage.clear();
+        location.reload();
         // When ready to go live, remove the alert and show a success message within this page. For example:
         // const element = document.getElementById('paypal-button-container');
         // element.innerHTML = '<h3>Thank you for your payment!</h3>';
